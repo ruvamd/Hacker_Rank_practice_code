09fa056aa0dcf93c19ade1b00ecfa58ae1a28014
vadim rusu <ruvamd@gmail.com>
2022-10-06 19:36:35 -0700

    .

--- Data Structures/Linked Lists/Print the Elements of a Linked List.py
+++ Data Structures/Linked Lists/Print the Elements of a Linked List.py
@@ -1,24 +1,15 @@
-# SinglyLinkedListNode:
-#     int data
-#     SinglyLinkedListNode next
 
-head=[2,16,13]
-# data=[16,13]
 
-def SinglyLinkedListNode(head): 
+def SinglyLinkedListNode(head):
     while head!=None:
         print(head.data)
-        head=head.next 
+        head=head.next
 
-print(SinglyLinkedListNode())
+print(SinglyLinkedListNode(2))
 
-        
-# SinglyLinkedListNode(16)
-# ih=2
-# id=[16,13]
 
-# def print_list(ih):
-#     while(ih!=None):
-#         print(ih.data)
-#         ih=ih.next
+
+
+
+
 
